f683a3ba584083802e89a28c9af7b8f5543ddb6b
Korotenko Alex <alexey.korotenko90@gmail.com>
2022-07-23 20:18:20 +0300

    new repo

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles, theme } from 'styles';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import Loader from 'components/Loader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Suspense fallback={<Loader />}>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
-    </Suspense>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
